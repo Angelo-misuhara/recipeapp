70e69e3a86d387182709abda91b733d5ee1c0c57
ogel <yocorangelo18@gmail.com>
2023-08-09 18:50:08 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import HomePage from './Layouts/HomePage'
+import Navbar from './Layouts/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='background'>
+        <Navbar/>
+        <HomePage/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layouts/HomePage.jsx
@@ -0,0 +1,14 @@
+
+const HomePage = () => {
+  return (
+    <>
+      <div className=" flex justify-center overflow-hidden w-screen">
+        <div className=" grid place-content-center border p-9 bg-[url('/img/backgroundpiccook.jpg')] rounded-md  w-[80rem] bg-repeat bg-cover">
+          <h1 className=" introsloggan text-center  font-black px-7 md:text-2xl font-[Preahvihear] text-[#765827]">Delightful Dishes, Effortlessly Discovered</h1>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
