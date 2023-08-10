142d9ac48a58d3fd04b75158707edf78e880ecad
ogel <yocorangelo18@gmail.com>
2023-08-10 17:28:50 +0800

    modified in main page

--- src/Layouts/HomePage.jsx
+++ src/Layouts/HomePage.jsx
@@ -3,8 +3,8 @@ const HomePage = () => {
   return (
     <>
       <div className=" flex justify-center overflow-hidden w-screen">
-        <div className=" grid place-content-center border p-9 bg-[url('/img/backgroundpiccook.jpg')] rounded-md  w-[80rem] bg-repeat bg-cover">
-          <h1 className=" introsloggan text-center  font-black px-7 md:text-2xl font-[Preahvihear] text-[#765827]">Delightful Dishes, Effortlessly Discovered</h1>
+        <div className=" grid place-content-center border p-9 bg-[url('/img/backgroundpiccook.jpg')] rounded-md  w-[78.5rem] h-[20rem] bg-repeat bg-cover">
+          <h1 className=" introsloggan text-center  font-black px-7 md:text-4xl font-[Preahvihear] text-[#765827]">Delightful Dishes, Effortlessly Discovered</h1>
           </div>
       </div>
     </>
--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const Navbar = () => {
 
-  const [isShow, setShow] = useState(true);
+  const [isShow, setShow] = useState(false);
   
   const ul=['Main', 'Pastry' ,'Protein']
   return (
